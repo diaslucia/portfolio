69faddc74d2f8392debe0e0ed871e57ec11bdb5c
Lucia <lucy_17_4@hotmail.com>
2022-03-30 11:45:41 -0300

    First commit

---
+++ src/components/homeBanner/index.js
@@ -0,0 +1,13 @@
+import styles from "./style.scss";
+
+const HomeBanner = () => {
+    return(
+        <div className="container">
+            <div className="whiteBorder">
+                <h1 className="title">Gatpo</h1>
+            </div>
+        </div>
+    );
+}
+
+export default HomeBanner;
---
+++ src/views/Home.js
@@ -0,0 +1,9 @@
+import HomeBanner from "../components/homeBanner/index";
+
+const Home = () => {
+    return(
+        <HomeBanner/>
+    );
+}
+
+export default Home;
