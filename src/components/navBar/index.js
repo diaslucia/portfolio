7b99f851dc9bf342b46d2e523ebf702a3b85c9e8
Lucia <lucy_17_4@hotmail.com>
2022-04-18 01:07:16 -0300

    Fix project mobile v

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,11 +1,13 @@
 import React,{ useState } from "react";
 import style from "./style.scss";
 
+/* Scroll */
 import { Link } from "react-scroll";
 
 /* Icons */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 
 const NavBar = ({ handleClick, t }) => {
     const [navbar,setNavbar] = useState(false);
@@ -19,6 +21,7 @@ const NavBar = ({ handleClick, t }) => {
         }   
     }
 
+
     const handleHamb = () => {
         setHamb(!hamb);
     }
@@ -28,11 +31,11 @@ const NavBar = ({ handleClick, t }) => {
     return(
         <nav className={navbar ? "nav activeNav" : "nav"}>
             <div className={hamb ? "navBarInactive" : "navBar"}>
-                <Link onClick={handleHamb} activeClass="active" className="link" to="home" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.home")}</Link>
-                <Link onClick={handleHamb} activeClass="active" className="link" to="about" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.about")}</Link>
-                <Link onClick={handleHamb} activeClass="active" className="link" to="skills" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.skills")}</Link>
-                <Link onClick={handleHamb} activeClass="active" className="link" to="projects" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.projects")}</Link>
-                <Link onClick={handleHamb} activeClass="active" className="link" to="contact" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.contact")}</Link>
+                <Link activeClass="active" className="link" to="home" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.home")}</Link>
+                <Link activeClass="active" className="link" to="about" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.about")}</Link>
+                <Link activeClass="active" className="link" to="skills" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.skills")}</Link>
+                <Link activeClass="active" className="link" to="projects" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.projects")}</Link>
+                <Link activeClass="active" className="link" to="contact" spy={true} smooth={true} offset={0} duration={1000}>{t("navbar.contact")}</Link>
             </div>
             <div className="navBarHamb" onClick={handleHamb}>
                 <FontAwesomeIcon className="icon" icon={faBars}/>
--- src/constants/projectCard/index.js
+++ src/constants/projectCard/index.js
@@ -5,6 +5,9 @@ import style from "./style.scss";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import "animate.css/animate.min.css";
 
+/* Scroll */
+import { Link } from "react-scroll";
+
 const ProjectCard = ({ title, tecnology, img, onClick }) => {
     const [cardHover,setCardHover] = useState("none");
 
@@ -17,16 +20,18 @@ const ProjectCard = ({ title, tecnology, img, onClick }) => {
     }
 
     return(
-        <AnimationOnScroll animateIn="animate__fadeIn">
-            <div className="cardContainer" onMouseOver={onHoverEnter} onMouseOut={onHoverOut} onClick={onClick}>
-                <div className="cardImgContainer" style={{ backgroundImage: `url(${img})` }}>
-                    <div className="cardHoverContainer" style={{ display: cardHover }}>
-                        <h2>{title}</h2>
-                        <p>{tecnology}</p>
-                    </div> 
+        <Link activeClass="active" to="projects" spy={true} smooth={true}>
+            <AnimationOnScroll animateIn="animate__fadeIn">
+                <div className="cardContainer" onMouseOver={onHoverEnter} onMouseOut={onHoverOut} onClick={onClick}>
+                    <div className="cardImgContainer" style={{ backgroundImage: `url(${img})` }}>
+                        <div className="cardHoverContainer" style={{ display: cardHover }}>
+                            <h2>{title}</h2>
+                            <p>{tecnology}</p>
+                        </div> 
+                    </div>
                 </div>
-            </div>
-        </AnimationOnScroll>
+            </AnimationOnScroll>
+        </Link>
     );
 }
 
